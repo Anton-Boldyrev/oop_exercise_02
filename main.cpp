60c6667991e18b274cd330eb4ddcb9076152c393
Anton-Boldyrev <antonbitee@gmail.com>
2019-11-14 17:19:40 +0300

    lab2

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "complex.hpp"
+#include <cmath>
+
+int main() {
+    complex a, b, ans;
+    std::cin >> a;
+    std::cin >> b;
+    std::cout << "First number : " << a << "\n";     
+    std::cout << "second number : " << b << "\n";
+    std::cout << " +    =  " << a + b << "\n";
+	std::cout << " -    =  " << a - b << "\n";
+	std::cout << " *    =  " << a * b << "\n";
+	std::cout << " /    =  " << a / b << "\n";
+    ans = a.conj(a);
+    std::cout << "conj  =  " << ans << "\n";
+    ans = b.conj(b);
+    std::cout << "conj  =  " << ans << "\n";
+    if ( a == b ) {
+        std::cout << "a    ==    b\n";    
+    } else {
+        std::cout << "a    !=    b\n";
+    } 
+}
