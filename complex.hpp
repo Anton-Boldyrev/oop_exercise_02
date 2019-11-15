2437e06ea244d6447f784dbe9f290925711691f1
Anton-Boldyrev <57538695+Anton-Boldyrev@users.noreply.github.com>
2019-11-15 18:48:58 +0300

    Lab2

---
+++ complex.hpp
@@ -0,0 +1,27 @@
+#ifndef __COMPLEX__
+#define __COMPLEX__
+#include <iostream>
+
+class complex {
+
+public:
+    complex();
+    complex(double a, double b);
+    complex conj(const complex& o) const;
+
+    friend complex operator + (const complex& , const complex& );
+    friend complex operator - (const complex& , const complex& );
+    friend complex operator * (const complex& , const complex& );
+    friend complex operator / (const complex& , const complex& );
+	friend std::ostream& operator << (std::ostream& out, const complex& complex);
+	friend void operator >> (std::istream &in, complex& complex);
+    friend bool operator == (const complex& , const complex& );
+
+private:
+    double r;
+    double fi;
+};
+
+complex operator ""_xn (long double first);
+
+#endif
